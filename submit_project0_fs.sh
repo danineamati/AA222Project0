e7f2764a3cf6c53aeedd3ccd7ab64281885b320d
Daniel Neamati <danineamati@gmail.com>
2023-04-07 11:28:46 -0700

    Slurm output and error redirect
    Redirect output and error to folder with custom naming conversion. It also marks the start and end of the run.

--- submit_project0_fs.sh
+++ submit_project0_fs.sh
@@ -4,6 +4,15 @@
 #SBATCH --time=0-00:10:00
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=proj0_fs
+#SBATCH --output=slurm_reports/slurm-%j-%x.out
+#SBATCH --error=slurm_reports/slurm-%j-%x.err
+
+# List the working directory, where it is running, and the date/time
+# for easier debugging/metadata
+pwd; hostname; date
 
 module load julia/1.8.0
 julia ./localtest.jl
+
+# List the time when it finished
+date
