71775ffed413c69801897661bae8ca5e4a2df019
Daniel Neamati <danineamati@gmail.com>
2023-04-07 11:45:55 -0700

    Redirect output to folder

--- submit_project0_fs.sh
+++ submit_project0_fs.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=0-00:10:00
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=proj0_fs
-#SBATCH --output=slurm-%j-%x.out
+#SBATCH --slurm_reports/output=slurm-%j-%x.out
 
 # List the working directory, where it is running, and the date/time
 # for easier debugging/metadata
