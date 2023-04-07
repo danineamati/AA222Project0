c260654888317479cacd441ccab1ff37beee8d0d
Daniel Neamati <danineamati@gmail.com>
2023-04-07 11:28:46 -0700

    Updated Slurm Report Handling

--- submit_project0_fs.sh
+++ submit_project0_fs.sh
@@ -4,6 +4,8 @@
 #SBATCH --time=0-00:10:00
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=proj0_fs
+#SBATCH --output=./slurm_reports/slurm-%j.out
+#SBATCH --error=./slurm_reports/slurm-%j.err
 
 module load julia/1.8.0
 julia ./localtest.jl
