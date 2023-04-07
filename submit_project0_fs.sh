3d4eec0202ad4e3ed10460772c79620c8bb77a29
Daniel Neamati <danineamati@gmail.com>
2023-04-07 11:23:22 -0700

    Farmshare SBATCH Attempt

---
+++ submit_project0_fs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --partition=normal
+#SBATCH --time=0-00:10:00
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=proj0_fs
+
+module load julia/1.8.0
+julia ./localtest.jl
